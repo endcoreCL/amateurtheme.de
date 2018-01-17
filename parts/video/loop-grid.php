b45ff8adf1da404772550c757189f1b83829f4b4
Christian Lang <cl@endcore.com>
2018-01-17 11:11:01 +0100

    added new image size to video grid

--- parts/video/loop-grid.php
+++ parts/video/loop-grid.php
@@ -1,11 +1,11 @@
-<div class="col-xxs12 col-xs-6 col-md-6 col-md-4 col-lg-3">
+<div class="col-xxs12 col-xs-6 col-md-4 col-md-4">
     <article <?php post_class('video-grid'); ?>>
         <a href="<?php the_permalink(); ?>">
             <?php
             /**
              * @TODO: Bildgröße anpassen
              */
-            the_post_thumbnail('full', array('class' => 'img-responsive'));
+            the_post_thumbnail('video_grid', array('class' => 'img-responsive'));
             ?>
         </a>
     </article>
