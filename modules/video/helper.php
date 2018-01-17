4cd041a824f3935f4cce14c3f0727bf3758a8266
Christian Lang <cl@endcore.com>
2018-01-17 17:59:06 +0100

    edited at_video_taxonomy_args

--- modules/video/helper.php
+++ modules/video/helper.php
@@ -11,11 +11,11 @@ add_image_size('video_grid', 345, 194, true);
 
 if ( ! function_exists('at_video_taxonomy_args') ) {
     /**
-     * edit taxonomy archive query for video_actor / video_category
+     * edit taxonomy archive query for video_actor / video_category / video_tags
      */
     add_filter('pre_get_posts', 'at_video_taxonomy_args');
     function at_video_taxonomy_args($query) {
-        if (($query->is_tax('video_actor') || $query->is_tax('video_category')) && $query->is_main_query()) {
+        if (($query->is_tax('video_actor') || $query->is_tax('video_category') || $query->is_tax('video_tags')) && $query->is_main_query()) {
             $query->set('posts_per_page', 12);
         }
 
