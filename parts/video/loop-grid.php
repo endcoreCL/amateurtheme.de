d5c691a1cdabac433d804908f3966b53f04f6c0b
Christian Lang <cl@endcore.com>
2018-01-17 12:00:08 +0100

    edited video grid layout

--- parts/video/loop-grid.php
+++ parts/video/loop-grid.php
@@ -1,12 +1,37 @@
+<?php global $post; $video = new AT_Video($post->ID); ?>
 <div class="col-xxs12 col-xs-6 col-md-4 col-md-4">
     <article <?php post_class('video-grid'); ?>>
-        <a href="<?php the_permalink(); ?>">
-            <?php
-            /**
-             * @TODO: Bildgröße anpassen
-             */
-            the_post_thumbnail('video_grid', array('class' => 'img-responsive'));
-            ?>
-        </a>
+        <div class="video-thumb-container">
+            <a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
+                <?php
+                /**
+                 * @TODO: Bildgröße anpassen
+                 */
+                the_post_thumbnail('video_grid', array('class' => 'img-responsive'));
+
+                $duration = $video->duration();
+                if($duration) {
+                    ?>
+                    <div class="video-duration"><?php echo $duration; ?></div>
+                    <?php
+                }
+                ?>
+            </a>
+        </div>
+
+        <div class="video-info">
+            <a class="video-title" href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
+                <?php echo $video->title(); ?>
+            </a>
+            <ul class="list-inline">
+                <li class="video-views">
+                    <i class="fa fa-eye"  aria-hidden="true"></i> <?php echo $video->views(); ?>
+                </li>
+                <li class="video-rating">
+                    <i class="fa fa-star" aria-hidden="true"></i> <?php echo $video->rating(); ?>
+                </li>
+            </ul>
+
+        </div>
     </article>
 </div>
