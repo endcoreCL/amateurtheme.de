c1b3fdbfd79d634d99aaefed278f94a1bd96dfdb
Christian Lang <cl@endcore.com>
2018-01-17 17:46:21 +0100

    added at_debug, at_error_log

--- library/helper/func.php
+++ library/helper/func.php
@@ -23,6 +23,39 @@ add_image_size('xcore_lg', 960, 0, false);
 add_image_size('xcore_md', 720, 0, false);
 add_image_size('xcore_sm', 540, 0, false);
 
+/**
+ * Memory Limit anpassen (Dashboard)
+ */
+ini_set( 'memory_limit', apply_filters( 'admin_memory_limit', -1) );
+
+if ( ! function_exists('at_debug') ) {
+    /**
+     * at_debug function.
+     *
+     * @param  arraa $var
+     * @return string
+     */
+    function at_debug($var) {
+        echo '<pre>';
+        print_r($var);
+        echo '</pre>';
+    }
+}
+
+if ( ! function_exists('at_error_log') ) {
+    /**
+     * at_error_log function.
+     *
+     * @param  arraa $var
+     * @return string
+     */
+    function at_error_log($var) {
+        if(WP_DEBUG_LOG) {
+            error_log($var);
+        }
+    }
+}
+
 if ( ! function_exists( 'xcore_get_image_sizes' ) ) {
     /**
      * Formatierte Ausgabe eines Arrays
@@ -149,5 +182,93 @@ if ( ! function_exists( 'xcore_tablet_detect' ) ) {
         } else {
             return false;
         }
+    }
+}
+
+if ( ! function_exists( 'at_attach_external_image' ) ) {
+    /**
+     * at_attach_external_image function.
+     *
+     * @param  string $url
+     * @param  int $post_id
+     * @param  string $thumb
+     * @param  string $filename
+     * @param  array $postdata
+     * @return boolean
+     */
+    function at_attach_external_image($url = null, $post_id = null, $thumb = null, $filename = null, $post_data = array()) {
+        if (!$url) return new WP_Error('missing', "Need a valid URL and post ID...");
+        require_once(ABSPATH . 'wp-admin/includes/file.php');
+        require_once(ABSPATH . 'wp-admin/includes/media.php');
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+        // Download file to temp location, returns full server path to temp file, ex; /home/user/public_html/mysite/wp-content/26192277_640.tmp
+        $tmp = download_url($url);
+
+        // If error storing temporarily, unlink
+        if (is_wp_error($tmp)) {
+            @unlink($file_array['tmp_name']);   // clean up
+            $file_array['tmp_name'] = '';
+            return $tmp; // output wp_error
+        }
+
+        preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $url, $matches);    // fix file filename for query strings
+
+        // fix for empty file extension
+        if (isset($matches[0])) {
+            $url_filename = basename($matches[0]);
+            $url_type = wp_check_filetype($url_filename);
+        } else {
+            $url_filename = $url;
+            $url_type['ext'] = 'jpg';
+        }
+
+        // determine file type (ext and mime/type)
+
+        // override filename if given, reconstruct server path
+        if (!empty($filename)) {
+            $filename = sanitize_file_name($filename);
+            $tmppath = pathinfo($tmp);                                                        // extract path parts
+            $new = $tmppath['dirname'] . "/" . $filename . "." . $tmppath['extension'];          // build new path
+            rename($tmp, $new);                                                                 // renames temp file on server
+            $tmp = $new;                                                                        // push new filename (in path) to be used in file array later
+        }
+
+        // assemble file data (should be built like $_FILES since wp_handle_sideload() will be using)
+        $file_array['tmp_name'] = $tmp;                                                         // full server path to temp file
+
+        if (!empty($filename)) {
+            $file_array['name'] = $filename . "." . $url_type['ext'];                           // user given filename for title, add original URL extension
+        } else {
+            $file_array['name'] = $url_filename;                                                // just use original URL filename
+        }
+
+        // set additional wp_posts columns
+        if (empty($post_data['post_title'])) {
+            $post_data['post_title'] = basename($url_filename, "." . $url_type['ext']);         // just use the original filename (no extension)
+        }
+
+        if($post_id) {
+            // make sure gets tied to parent
+            if (empty($post_data['post_parent'])) {
+                $post_data['post_parent'] = $post_id;
+            }
+        }
+
+        // do the validation and storage stuff
+        $att_id = media_handle_sideload($file_array, $post_id, null, $post_data);             // $post_data can override the items saved to wp_posts table, like post_mime_type, guid, post_parent, post_title, post_content, post_status
+
+        // If error storing permanently, unlink
+        if (is_wp_error($att_id)) {
+            @unlink($file_array['tmp_name']);   // clean up
+            return $att_id; // output wp_error
+        }
+
+        // set as post thumbnail if desired
+        if ($thumb) {
+            set_post_thumbnail($post_id, $att_id);
+        }
+
+        return $att_id;
     }
 }
