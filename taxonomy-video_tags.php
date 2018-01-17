78f6ec5deba483bee375fc5c91159fc14588a5b6
Christian Lang <cl@endcore.com>
2018-01-17 17:58:31 +0100

    added video_tags template

---
+++ taxonomy-video_tags.php
@@ -0,0 +1,49 @@
+<?php
+get_header();
+
+/**
+ * Vars
+ */
+$queried_object = get_queried_object();
+$term_id = $queried_object->term_id;
+?>
+
+<div id="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <div id="sidebar">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+
+            <div class="col-10">
+                <div id="content">
+                    <h1><?php single_term_title(); ?></h1>
+
+                    <?php
+                    $description = term_description();
+                    if ($description && !is_paged()) {
+                        echo '<div class="category-description">' . $description . '</div>';
+                    }
+                    ?>
+
+                    <div class="category-videos">
+                        <?php
+                        if (have_posts()) :
+                            echo '<div class="row">';
+                            while (have_posts()) : the_post();
+                                get_template_part('parts/video/loop', 'grid');
+                            endwhile;
+                            echo '</div>';
+                            echo at_pagination(3);
+                        endif;
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
