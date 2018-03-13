8429a9bb48ad43749d0be2bf5ff3836b16f691ed
Christian Lang <cl@endcore.com>
2018-03-13 14:35:01 +0100

    edited video loops

--- parts/video/loop-grid.php
+++ parts/video/loop-grid.php
@@ -1,36 +1,32 @@
 <?php global $post; $video = new AT_Video($post->ID); ?>
-<div class="col-xxs12 col-xs-6 col-md-4 col-md-4 col-lg-3">
-    <article <?php post_class('video-grid'); ?>>
-        <div class="video-thumb-container">
-            <a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
-                <?php
-                /**
-                 * @TODO: Bildgröße anpassen
-                 */
-                the_post_thumbnail('video_grid', array('class' => 'img-fluid'));
 
-                $duration = $video->duration();
-                if($duration) {
-                    ?>
-                    <div class="video-duration"><?php echo $duration; ?></div>
-                    <?php
-                }
-                ?>
-            </a>
-        </div>
+	<div class="card card-video card-video-grid">
+		<?php
+		$duration = $video->duration();
+		if($duration) { ?>
+			<span class="badge badge-dark">
+				<?php echo $duration; ?>
+			</span>
+		<?php } ?>
 
-        <div class="video-info">
-            <a class="video-title" href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
-                <?php echo $video->title(); ?>
-            </a>
-            <ul class="list-inline">
-                <li class="list-inline-item video-views">
-                    <i class="fa fa-eye"  aria-hidden="true"></i> <?php echo $video->views(); ?>
-                </li>
-                <li class="list-inline-item video-rating">
-                    <i class="fa fa-star" aria-hidden="true"></i> <?php echo $video->rating(); ?>
-                </li>
-            </ul>
-        </div>
-    </article>
-</div>
+		<a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
+			<?php the_post_thumbnail('video_grid', array('class' => 'img-fluid card-img-top')); ?>
+		</a>
+
+		<div class="card-body">
+			<h3 class="card-title">
+				<a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
+					<?php echo $video->title(); ?>
+				</a>
+			</h3>
+			<ul class="list-inline list-meta">
+				<li class="list-inline-item">
+					<i class="fa fa-eye" aria-hidden="true"></i> <?php echo $video->views(); ?>
+				</li>
+				<li class="list-inline-item">
+					<i class="fa fa-star" aria-hidden="true"></i> <?php echo $video->rating(); ?>
+				</li>
+			</ul>
+		</div>
+	</div>
+
