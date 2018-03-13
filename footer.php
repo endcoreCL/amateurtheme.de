606831036c1c182629151067c15faac8cca2c717
Christian Lang <cl@endcore.com>
2018-03-13 14:37:46 +0100

    edited footer

--- footer.php
+++ footer.php
@@ -1,19 +1,18 @@
-    </body>
+    	</div>
+	</body>
 
-    <?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 
-    <?php
-    /**
-     * Support old Browsers (eg IE6-9)
-     */
-    if(get_field('src_ie_support', 'options')) {
-        ?>
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-        <?php
-    }
-    ?>
+	<?php
+	/* Support old Browsers (eg IE6-9) */
+	if(get_field('src_ie_support', 'options')) {
+		?>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<?php
+	}
+	?>
 </html>
