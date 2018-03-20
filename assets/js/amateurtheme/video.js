660847f7a82ee53a241ec84919cbd5859f9f5a53
Christian Lang <cl@endcore.com>
2018-03-20 07:38:42 +0100

    added preview videos

---
+++ assets/js/amateurtheme/video.js
@@ -0,0 +1,5 @@
+//
+//player.play();
+jQuery(document).ready(function () {
+    const player = new Plyr('#player');
+});
--- single-video.php
+++ single-video.php
@@ -14,15 +14,38 @@ $video = new AT_Video(get_the_ID());
                         <div class="col-10">
                             <div class="video-thumb-container">
                                 <?php
-                                $external_url = $video->external_url();
-                                if($external_url) {
+                                // preview videos
+                                $preview_webm = $video->preview_video('webm');
+                                $preview_mp4 = $video->preview_video('mp4');
+
+                                if($preview_webm || $preview_mp4) {
+                                    $poster = $video->thumbnail_url();
                                     ?>
-                                    <a href="<?php echo $external_url; ?>" title="<?php $video->title(); ?>" target="_blank" rel="nofollow">
-                                        <?php echo $video->thumbnail('full', array('class' => 'img-fluid', 'style' => 'width: 100%; height: auto;')); ?>
-                                    </a>
+                                    <video poster="<?php echo $poster; ?>" id="player" playsinline controls data-plyr-config='{"loadSprite" : "<?php echo get_template_directory_uri(); ?>/assets/img/plyr.svg"}'>
+                                        <?php if($preview_mp4) { ?><source src="<?php echo $preview_mp4; ?>" type="video/mp4"><?php } ?>
+                                        <?php if($preview_webm) { ?><source src="<?php echo $preview_webm; ?>" type="video/webm"><?php } ?>
+                                    </video>
                                     <?php
                                 } else {
-                                    echo $video->thumbnail();
+	                                // thumbnail
+	                                $external_url = $video->external_url();
+	                                if ( $external_url ) {
+		                                ?>
+                                        <a href="<?php echo $external_url; ?>" title="<?php $video->title(); ?>" target="_blank" rel="nofollow">
+			                                <?php
+                                            echo $video->thumbnail( 'full', array(
+				                                'class' => 'img-fluid',
+				                                'style' => 'width: 100%; height: auto;'
+			                                ) );
+                                            ?>
+                                        </a>
+		                                <?php
+	                                } else {
+                                        echo $video->thumbnail( 'full', array(
+                                            'class' => 'img-fluid',
+                                            'style' => 'width: 100%; height: auto;'
+                                        ) );
+	                                }
                                 }
                                 ?>
                             </div>
