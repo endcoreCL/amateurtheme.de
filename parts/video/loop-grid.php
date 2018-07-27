4180771f31a1bb27513ccc2d6dee2d64c052ead8
Christian Lang <cl@endcore.com>
2018-07-27 14:05:44 +0200

    edited video grid

--- parts/video/loop-grid.php
+++ parts/video/loop-grid.php
@@ -1,32 +1,34 @@
-<?php global $post; $video = new AT_Video($post->ID); ?>
+<?php
+global $post;
+$video = new AT_Video($post->ID);
+?>
+<div class="card card-video card-video-grid">
+    <?php
+    $duration = $video->duration();
+    if($duration) { ?>
+        <span class="badge badge-dark">
+            <?php echo $duration; ?>
+        </span>
+    <?php } ?>
 
-	<div class="card card-video card-video-grid">
-		<?php
-		$duration = $video->duration();
-		if($duration) { ?>
-			<span class="badge badge-dark">
-				<?php echo $duration; ?>
-			</span>
-		<?php } ?>
+    <a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
+        <?php the_post_thumbnail('video_grid', array('class' => 'img-fluid card-img-top')); ?>
+    </a>
 
-		<a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
-			<?php the_post_thumbnail('video_grid', array('class' => 'img-fluid card-img-top')); ?>
-		</a>
-
-		<div class="card-body">
-			<h3 class="card-title">
-				<a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
-					<?php echo $video->title(); ?>
-				</a>
-			</h3>
-			<ul class="list-inline list-meta">
-				<li class="list-inline-item">
-					<i class="fa fa-eye" aria-hidden="true"></i> <?php echo $video->views(); ?>
-				</li>
-				<li class="list-inline-item">
-					<i class="fa fa-star" aria-hidden="true"></i> <?php echo $video->rating(); ?>
-				</li>
-			</ul>
-		</div>
-	</div>
+    <div class="card-body">
+        <h3 class="card-title">
+            <a href="<?php echo $video->permalink(); ?>" title="<?php echo $video->title(); ?>">
+                <?php echo $video->title(); ?>
+            </a>
+        </h3>
+        <ul class="list-inline list-meta">
+            <li class="list-inline-item">
+                <i class="fa fa-eye" aria-hidden="true"></i> <?php echo $video->views(); ?>
+            </li>
+            <li class="list-inline-item">
+                <i class="fa fa-star" aria-hidden="true"></i> <?php echo $video->rating(); ?>
+            </li>
+        </ul>
+    </div>
+</div>
 
