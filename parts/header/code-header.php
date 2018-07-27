de9ef8fee30bec9569a5a53519f223fa41981b8c
Christian Lang <cl@endcore.com>
2018-07-27 13:15:13 +0200

    edited header layout

--- parts/header/code-header.php
+++ parts/header/code-header.php
@@ -1,60 +1,95 @@
 <?php
 $layout = new xCORE_Layout();
-$navbar_pos = $layout->navbar_pos();
+$logo_pos = $layout->logo_pos();
 $banner = get_field('design_header_banner', 'options');
 $search = get_field('design_header_search', 'options');
-
-if($navbar_pos == 'top') {
-	?>
-	<header id="header">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-3 align-self-center">
-					<a class="header-brand" href="<?php echo home_url(); ?>"
-					   title="<?php bloginfo( 'name' ); ?>"><?php echo $layout->logo(); ?></a>
-				</div>
-				<?php
+$search_pos = get_field('design_header_search_pos', 'options');
+?>
+<header id="header">
+    <div class="container">
+        <div class="row">
+            <?php
+            if($logo_pos == 'top') {
+                ?>
+                <div class="col-sm-3 align-self-center">
+                    <a class="header-brand" href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>">
+                        <?php echo $layout->logo(); ?>
+                    </a>
+                </div>
+                <?php
                 if ( $banner ) {
-                    if( $search ) {
-	                    ?>
+                    if( $search && $search_pos == 'top' ) {
+                        ?>
                         <div class="col-sm-6 align-self-center">
                             <div class="header-search">
-			                    <?php get_search_form(); ?>
+                                <?php get_search_form(); ?>
                             </div>
                         </div>
                         <div class="col-sm-3 align-self-center">
                             <div class="header-text">
-			                    <?php echo $banner; ?>
+                                <?php echo $banner; ?>
                             </div>
                         </div>
-	                    <?php
+                        <?php
                     } else {
                         ?>
                         <div class="col-sm-9 align-self-center">
                             <div class="header-text">
-			                    <?php echo $banner; ?>
+                                <?php echo $banner; ?>
                             </div>
                         </div>
                         <?php
                     }
-                } else if($search) { ?>
-					<div class="col-sm-9 align-self-center">
-						<div class="header-search">
-							<?php get_search_form(); ?>
-						</div>
-					</div>
-				<?php } ?>
-			</div>
-		</div>
-	</header>
-	<?php
-}
-?>
+                } else if($search && $search_pos == 'top') { ?>
+                    <div class="col-sm-6 offset-sm-3">
+                        <div class="header-search">
+                            <?php get_search_form(); ?>
+                        </div>
+                    </div>
+                <?php
+                }
+            } else {
+                if ( $banner ) {
+                    if( $search && $search_pos == 'top' ) {
+                        ?>
+                        <div class="col-sm-6 align-self-center">
+                            <div class="header-search">
+                                <?php get_search_form(); ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 align-self-center">
+                            <div class="header-text">
+                                <?php echo $banner; ?>
+                            </div>
+                        </div>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="col-sm-12 align-self-center">
+                            <div class="header-text">
+                                <?php echo $banner; ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                } else if($search && $search_pos == 'top') { ?>
+                    <div class="col-sm-6 offset-sm-6">
+                        <div class="header-search">
+                            <?php get_search_form(); ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+</header>
 
 <nav class="<?php echo $layout->navbar_wrapper_classes(); ?>" id="navigation">
 	<div class="container">
 		<?php
-		if($navbar_pos == 'inline') {
+		if($logo_pos == 'inline') {
 			?>
 			<a class="navbar-brand" href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php echo $layout->logo(); ?></a>
 			<?php
@@ -78,6 +113,10 @@ if($navbar_pos == 'top') {
 				'walker'          => new xcore_nav_walker()
 			)
 		);
+
+		if($search_pos == 'inline') {
+			get_search_form();
+        }
 		?>
 	</div>
 </nav>
