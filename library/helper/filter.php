00c36b9efb4495ff9e31e2daef8ddcac8dd277a9
Christian Lang <cl@endcore.com>
2019-03-19 12:52:03 +0100

    edited body class function

--- library/helper/filter.php
+++ library/helper/filter.php
@@ -84,47 +84,37 @@ function xcore_media_mime_types($mimes) {
     return $mimes;
 }
 
-/**
- * Browser & OS Body-Classes
- */
-add_filter('body_class', 'xcore_browser_body_class');
-function xcore_browser_body_class($classes) {
-    global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
-    if($is_lynx) $classes[] = 'lynx';
-    elseif($is_gecko) $classes[] = 'gecko';
-    elseif($is_opera) $classes[] = 'opera';
-    elseif($is_NS4) $classes[] = 'ns4';
-    elseif($is_safari) $classes[] = 'safari';
-    elseif($is_chrome) $classes[] = 'chrome';
-    elseif($is_IE) {
-        $classes[] = 'ie';
-        if(preg_match('/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version))
-            $classes[] = 'ie'.$browser_version[1];
-    } else $classes[] = 'unknown';
-    if($is_iphone) $classes[] = 'iphone';
-    if ( stristr( $_SERVER['HTTP_USER_AGENT'],"android") ) {
-        $classes[] = 'android';
-    } else if ( stristr( $_SERVER['HTTP_USER_AGENT'],"mac") ) {
-        $classes[] = 'osx';
-    } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"linux") ) {
-        $classes[] = 'linux';
-    } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"windows") ) {
-        $classes[] = 'windows';
-    }
+if ( ! function_exists( 'at_browser_body_class' ) ) {
+    /**
+     * Browser & OS Body-Classes
+     */
+    add_filter( 'body_class', 'at_browser_body_class' );
+    function at_browser_body_class( $classes ) {
+        global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+        if($is_lynx) $classes[] = 'lynx';
+        elseif($is_gecko) $classes[] = 'gecko';
+        elseif($is_opera) $classes[] = 'opera';
+        elseif($is_NS4) $classes[] = 'ns4';
+        elseif($is_safari) $classes[] = 'safari';
+        elseif($is_chrome) $classes[] = 'chrome';
+        elseif($is_IE) {
+            $classes[] = 'ie';
+            if(preg_match('/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version))
+                $classes[] = 'ie'.$browser_version[1];
+        } else $classes[] = 'unknown';
+        if($is_iphone) $classes[] = 'iphone';
+        if ( stristr( $_SERVER['HTTP_USER_AGENT'],"android") ) {
+            $classes[] = 'android';
+        } else if ( stristr( $_SERVER['HTTP_USER_AGENT'],"mac") ) {
+            $classes[] = 'osx';
+        } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"linux") ) {
+            $classes[] = 'linux';
+        } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"windows") ) {
+            $classes[] = 'windows';
+        }
 
-    if(xcore_phone_detect()) {
-        $classes[] = 'is-phone';
+        return $classes;
     }
-
-    if(xcore_tablet_detect()) {
-        $classes[] = 'is-tablet';
-    }
-
-    if(!xcore_phone_detect() && !xcore_tablet_detect()) {
-        $classes[] = 'is-desktop';
-    }
-
-    return $classes;
 }
 
 /**
