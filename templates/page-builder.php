09a8abd26cd48fa1e49a3a2357878b15be152dcf
Christian Lang <cl@endcore.com>
2019-03-19 12:53:10 +0100

    edited page builder (prettify html)

--- templates/page-builder.php
+++ templates/page-builder.php
@@ -17,38 +17,39 @@ get_header(); ?>
                     /**
                      * Feld: Textarea
                      */
-                    if(get_row_layout() == 'page_builder_textarea') :
-			            $items = get_sub_field('editor');
+                    if( get_row_layout() == 'page_builder_textarea' ) :
+			            $items = get_sub_field( 'editor' );
 
 	                    $attributes = array(
-		                    'id' => array(get_sub_field('id')),
-		                    'class' => array('section', 'textarea', 'textarea-row-' . get_sub_field('rows'), 'item-' . $i),
+		                    'id' => array( get_sub_field( 'id' ) ),
+		                    'class' => array( 'section', 'textarea', 'textarea-row-' . get_sub_field( 'rows' ), 'item-' . $i ),
 		                    'style' => array(),
 	                    );
 
-	                    if(get_sub_field('class')) {
-		                    $attributes['class'][] = get_sub_field('class');
+	                    if( get_sub_field( 'class' ) ) {
+		                    $attributes['class'][] = get_sub_field( 'class' );
 	                    }
 
-	                    if(get_sub_field('padding') == '1') {
+	                    if( get_sub_field( 'padding' ) == '1' ) {
 		                    $attributes['class'][] = 'no-padding';
 	                    }
 
-	                    if(get_sub_field('id')) {
-		                    $attributes['class'][] = 'id-' . get_sub_field('id');
+	                    if( get_sub_field('id' ) ) {
+		                    $attributes['class'][] = 'id-' . get_sub_field( 'id' );
 	                    }
 
-	                    if(get_sub_field('bgcolor')) {
-		                    $attributes['style'][] = 'background-color: ' . get_sub_field('bgcolor') . ';';
+	                    if( get_sub_field( 'bgcolor' ) ) {
+		                    $attributes['style'][] = 'background-color: ' . get_sub_field( 'bgcolor' ) . ';';
 	                    }
-                        if($items) {
-	                        $count = count($items);
+
+                        if( $items ) {
+	                        $count = count( $items );
 	                        ?>
-                            <div <?php at_attribute_array_html($attributes); ?>>
+                            <div <?php echo at_attribute_array_html( $attributes ); ?>>
                                 <div class="row">
                                     <?php
-                                    foreach($items as $item) {
-                                        echo at_pb_render_editor($item, $count);
+                                    foreach( $items as $item ) {
+                                        echo at_pb_render_editor( $item, $count );
                                     }
                                     ?>
                                 </div>
@@ -60,30 +61,30 @@ get_header(); ?>
 	                /**
 	                 * Feld: Videos
 	                 */
-	                if(get_row_layout() == 'page_builder_videos') :
-                        $text = get_sub_field('text');
-                        $type = get_sub_field('type');
+	                if( get_row_layout() == 'page_builder_videos' ) :
+                        $text = get_sub_field( 'text' );
+                        $type = get_sub_field( 'type' );
 
 		                $attributes = array(
-			                'id' => array(get_sub_field('id')),
-			                'class' => array('section', 'textarea', 'textarea-row-' . get_sub_field('rows'), 'item-' . $i),
+			                'id' => array( get_sub_field( 'id' ) ),
+			                'class' => array( 'section', 'videos', 'item-' . $i ),
 			                'style' => array(),
 		                );
 
-		                if(get_sub_field('class')) {
-			                $attributes['class'][] = get_sub_field('class');
+		                if( get_sub_field( 'class' ) ) {
+			                $attributes['class'][] = get_sub_field( 'class' );
 		                }
 
-		                if(get_sub_field('padding') == '1') {
+		                if( get_sub_field( 'padding' ) == '1' ) {
 			                $attributes['class'][] = 'no-padding';
 		                }
 
-		                if(get_sub_field('id')) {
-			                $attributes['class'][] = 'id-' . get_sub_field('id');
+		                if( get_sub_field( 'id' ) ) {
+			                $attributes['class'][] = 'id-' . get_sub_field( 'id' );
 		                }
 
-		                if(get_sub_field('bgcolor')) {
-			                $attributes['style'][] = 'background-color: ' . get_sub_field('bgcolor') . ';';
+		                if( get_sub_field( 'bgcolor' ) ) {
+			                $attributes['style'][] = 'background-color: ' . get_sub_field( 'bgcolor' ) . ';';
 		                }
 
 		                $args = array(
@@ -92,17 +93,17 @@ get_header(); ?>
                             'tax_query' => array()
                         );
 
-		                if($type == 'latest') {
-                            $count = get_sub_field('latest_count');
-                            $actor = get_sub_field('latest_actor');
-                            $category = get_sub_field('latest_category');
-                            $tags = get_sub_field('latest_tags');
+		                if( $type == 'latest' ) {
+                            $count = get_sub_field( 'latest_count' );
+                            $actor = get_sub_field( 'latest_actor' );
+                            $category = get_sub_field( 'latest_category' );
+                            $tags = get_sub_field( 'latest_tags' );
 
-                            if($count) {
+                            if( $count ) {
                                 $args['posts_per_page'] = $count;
                             }
 
-                            if($actor) {
+                            if( $actor ) {
                                 $args['tax_query'][] = array(
                                     'taxonomy' => 'video_actor',
                                     'terms' => $actor,
@@ -110,7 +111,7 @@ get_header(); ?>
                                 );
                             }
 
-			                if($category) {
+			                if( $category ) {
 				                $args['tax_query'][] = array(
 					                'taxonomy' => 'video_category',
 					                'terms' => $category,
@@ -118,39 +119,39 @@ get_header(); ?>
 				                );
 			                }
 
-			                if($tags) {
+			                if( $tags ) {
 				                $args['tax_query'][] = array(
 					                'taxonomy' => 'video_tags',
 					                'terms' => $tags,
 					                'field' => 'term_id'
 				                );
 			                }
-                        } else if($type == 'post__in') {
-		                    $ids = get_sub_field('post__in_video');
+                        } else if( $type == 'post__in' ) {
+		                    $ids = get_sub_field( 'post__in_video' );
 
-		                    if($ids) {
+		                    if( $ids ) {
 		                        $args['post__in'] = $ids;
-		                        $args['posts_per_page'] = count($ids);
+		                        $args['posts_per_page'] = count( $ids );
                             }
                         }
 
-                        $videos = new WP_Query($args);
+                        $videos = new WP_Query( $args );
 
-		                if($videos->have_posts()) {
+		                if( $videos->have_posts() ) {
 			                ?>
-                            <div <?php at_attribute_array_html($attributes); ?>>
+                            <div <?php echo at_attribute_array_html( $attributes ); ?>>
                                 <?php
-                                if($text) {
+                                if( $text ) {
                                     echo $text;
                                 }
                                 ?>
                                 <div id="video-list">
                                     <div class="card-deck">
                                         <?php
-                                        while($videos->have_posts()) {
+                                        while( $videos->have_posts() ) {
 	                                        $videos->the_post();
 
-                                            get_template_part('parts/video/loop', 'card');
+                                            get_template_part( 'parts/video/loop', 'card' );
                                         }
 
                                         wp_reset_query();
@@ -168,9 +169,9 @@ get_header(); ?>
 				$output = ob_get_contents();
 				ob_end_clean();
 
-				remove_filter('the_content', 'wpautop');
-				echo apply_filters('the_content', $output);
-				add_filter('the_content', 'wpautop');
+				remove_filter( 'the_content', 'wpautop' );
+				echo apply_filters(' the_content', $output );
+				add_filter( 'the_content', 'wpautop' );
             endif;
 			?>
 		</div>
