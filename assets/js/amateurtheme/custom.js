5b02f90ee89f33db3be6c4a9b5e6cbfcbc8c6625
endcoreCL <cl@endcore.com>
2019-03-22 10:21:50 +0100

    edited custom js

--- assets/js/amateurtheme/custom.js
+++ assets/js/amateurtheme/custom.js
@@ -7,6 +7,54 @@ jQuery(document).ready(function () {
 });
 
 /**
+ * Navigation Dropdown
+ */
+jQuery( document ).ready( function () {
+    jQuery( '[data-toggle="dropdown"]' ).on( 'click' , function () {
+        jQuery( this ).find( 'i' ).toggleClass( 'fa-angle-down fa-angle-up' );
+    } );
+
+    jQuery( '.navbar ul.dropdown-menu [data-toggle="dropdown"]' ).on( 'click', function( event ) {
+        event.preventDefault();
+        event.stopPropagation();
+        jQuery( this ).parent().siblings().removeClass( 'show' );
+        jQuery( this ).parent().toggleClass( 'show' );
+        jQuery( this ).parent().find( '.dropdown-menu:first' ).toggleClass( 'show' );
+    } );
+} );
+
+/**
+ * Navigation collapse
+ */
+jQuery( document ).ready( function (e) {
+    jQuery('#navbarNav').on('show.bs.collapse', function (e) {
+        var button = jQuery('[data-target="#navbarNav"]');
+
+        var value = button.html();
+        var toggle_text = button.attr( 'data-toggle-text' );
+
+        if (toggle_text) {
+            button.html( toggle_text ).attr( 'data-toggle-text', value.trim() );
+        }
+    } );
+
+    jQuery('#navbarNav').on('hide.bs.collapse', function (e) {
+        var button = jQuery('[data-target="#navbarNav"]');
+
+        var value = button.html();
+        var toggle_text = button.attr( 'data-toggle-text' );
+
+        if (toggle_text) {
+            button.html( toggle_text ).attr( 'data-toggle-text', value.trim() );
+        }
+    } );
+
+    jQuery('#navbarNav').on('hidden.bs.collapse', function () {
+
+    } );
+} );
+
+/**
  * Social share
  */
 function social_share(elem, m) {
@@ -50,4 +98,22 @@ jQuery(document).ready(function($) {
 
         $redirectForm.submit();
     });
+});
+
+/**
+ * Alert Shortcode
+ */
+jQuery( document ).ready( function () {
+    if ( jQuery('.xcore-alert a').length > 0 ) {
+        jQuery('.xcore-alert a:not(.btn)').addClass('alert-link');
+    }
+});
+
+/**
+ * Auto responsive embed youtube videos
+ */
+jQuery( document ).ready( function() {
+    if ( jQuery('iframe[src*="youtube"]').length > 0 ) {
+        jQuery('iframe[src*="youtube"]').wrap('<div class="embed-responsive embed-responsive-16by9">');
+    }
 });
