bd6f51ced556f803feeceda7a5c2077366e133d7
endcoreCL <cl@endcore.com>
2020-02-19 13:28:22 +0100

    added acf fields by json

--- library/helper/acf-helper.php
+++ library/helper/acf-helper.php
@@ -2,12 +2,12 @@
 /**
  * ACF Functions
  *
- * @author		Christian Lang
- * @version		1.0
- * @category	helper
+ * @author        Christian Lang
+ * @version        1.0
+ * @category    helper
  */
 
-define( 'ACF_LITE' , false );
+define( 'ACF_LITE', false );
 define( 'ACFGFS_API_KEY', 'AIzaSyBOkk5G9qoAGJ21lNVVANdAqT0JmIrbHcI' );
 
 if ( ! function_exists( 'at_acf_settings_path' ) ) {
@@ -15,12 +15,15 @@ if ( ! function_exists( 'at_acf_settings_path' ) ) {
      * Set acf settings path
      *
      * @param $path
+     *
      * @return string
      */
 
     add_filter( 'acf/settings/path', 'at_acf_settings_path' );
-    function at_acf_settings_path( $path ) {
+    function at_acf_settings_path ( $path )
+    {
         $path = AT_PLUGINS . '/acf/core';
+
         return $path;
     }
 }
@@ -30,13 +33,39 @@ if ( ! function_exists( 'at_acf_settings_dir' ) ) {
      * Set acf settings dir
      *
      * @param $dir
+     *
      * @return string
      */
     add_filter( 'acf/settings/dir', 'at_acf_settings_dir' );
-    function at_acf_settings_dir( $dir ) {
+    function at_acf_settings_dir ( $dir )
+    {
         $dir = get_template_directory_uri() . '/library/plugins/acf/core/';
+
         return $dir;
     }
+}
+
+add_filter( 'acf/settings/save_json', 'xcore_acf_json_save_point' );
+function xcore_acf_json_save_point ( $path )
+{
+    // update path
+    $path = AT_PLUGINS . '/acf/fields';
+
+    // return
+    return $path;
+}
+
+add_filter( 'acf/settings/load_json', 'xcore_acf_json_load_point' );
+function xcore_acf_json_load_point ( $paths )
+{
+    // remove original path (optional)
+    unset( $paths[0] );
+
+    // append path
+    $paths[] = AT_PLUGINS . '/acf/fields';
+
+    // return
+    return $paths;
 }
 
 /**
@@ -50,44 +79,35 @@ if ( ! function_exists( 'at_acf_option_pages' ) ) {
      * Add acf option pages
      */
     add_action( 'init', 'at_acf_option_pages' );
-    function at_acf_option_pages() {
+    function at_acf_option_pages ()
+    {
         if ( function_exists( 'acf_add_options_page' ) ) {
-            acf_add_options_page(
-                array(
+            acf_add_options_page( array(
                     'page_title' => __( 'Framework', 'xcore' ),
                     'menu_title' => 'Framework',
-                    'icon_url' => 'dashicons-heart',
+                    'icon_url'   => 'dashicons-heart',
                     'capability' => 'manage_options'
-                )
-            );
+                ) );
 
-            acf_add_options_sub_page(
-                array(
-                    'title' => __( 'Allgemein', 'xcore' ),
+            acf_add_options_sub_page( array(
+                    'title'  => __( 'Allgemein', 'xcore' ),
                     'parent' => 'acf-options-framework'
-                )
-            );
+                ) );
 
-            acf_add_options_sub_page(
-                array(
-                    'title' => __( 'Design', 'xcore' ),
+            acf_add_options_sub_page( array(
+                    'title'  => __( 'Design', 'xcore' ),
                     'parent' => 'acf-options-framework'
-                )
-            );
+                ) );
 
-            acf_add_options_sub_page(
-                array(
-                    'title' => __( 'Blog', 'xcore' ),
+            acf_add_options_sub_page( array(
+                    'title'  => __( 'Blog', 'xcore' ),
                     'parent' => 'acf-options-framework'
-                )
-            );
+                ) );
 
-            acf_add_options_sub_page(
-                array(
-                    'title' => __( 'Videos', 'xcore' ),
+            acf_add_options_sub_page( array(
+                    'title'  => __( 'Videos', 'xcore' ),
                     'parent' => 'acf-options-framework'
-                )
-            );
+                ) );
         }
     }
 }
