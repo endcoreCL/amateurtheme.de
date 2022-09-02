3ac40d9335e595f4882c91be6c64009df97520b5
endcoreCL <cl@endcore.com>
2022-09-02 10:54:57 +0200

    fixed big7 categories import

--- modules/import/_classes/AT_Import_Big7_Crawler.php
+++ modules/import/_classes/AT_Import_Big7_Crawler.php
@@ -11,213 +11,276 @@
  */
 class AT_Import_Big7_Crawler
 {
-    public function __construct ()
-    {
-        // folder
-        $upload_dir   = wp_upload_dir();
-        $this->folder = $upload_dir['basedir'] . '/big7';
+	public function __construct()
+	{
+		// folder
+		$upload_dir   = wp_upload_dir();
+		$this->folder = $upload_dir['basedir'] . '/big7';
 
-        $this->wmb          = get_option( 'at_big7_wmb' );
-        $this->security_key = get_option( 'at_big7_security_key' );
-        $this->videos       = 'https://cash.big7.com/xml_export.php?wmb=' . $this->wmb . '&security_key=' . $this->security_key . '&file=all_amateurs_videos&format=json&dl';
-        $this->amateure     = 'https://cash.big7.com/xml_export.php?wmb=' . $this->wmb . '&security_key=' . $this->security_key . '&file=all_amateurs&format=json&dl';
+		$this->wmb          = get_option( 'at_big7_wmb' );
+		$this->security_key = get_option( 'at_big7_security_key' );
+		$this->videos       = 'https://cash.big7.com/xml_export.php?wmb=' . $this->wmb . '&security_key=' . $this->security_key . '&file=all_amateurs_videos&format=json&dl';
+		$this->amateure     = 'https://cash.big7.com/xml_export.php?wmb=' . $this->wmb . '&security_key=' . $this->security_key . '&file=all_amateurs&format=json&dl';
+		$this->categories   = 'https://cash.big7.com/xml_export.php?wmb=' . $this->wmb . '&security_key=' . $this->security_key . '&file=video_categories&format=json';
 
-        $this->json_result = array();
-    }
+		$this->json_result = array();
+	}
 
-    function json_callback ( $item )
-    {
-        $this->json_result[] = $item;
-    }
+	function json_callback( $item )
+	{
+		$this->json_result[] = $item;
+	}
 
-    function get ( $params = array() )
-    {
-        if ( $params['type'] == 'videos' ) {
-            $url = $this->videos;
-        }
+	function get( $params = array() )
+	{
+		if ( $params['type'] == 'videos' ) {
+			$url = $this->videos;
+		}
 
-        $curl = curl_init();
-        curl_setopt( $curl, CURLOPT_URL, $url );
-        curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, 1 );
-        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+		$curl = curl_init();
+		curl_setopt( $curl, CURLOPT_URL, $url );
+		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, 1 );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
 
-        $response = curl_exec( $curl );
+		$response = curl_exec( $curl );
 
-        if ( curl_errno( $curl ) ) {
-            return 'Curl error: ' . curl_error( $curl );
-        }
+		if ( curl_errno( $curl ) ) {
+			return 'Curl error: ' . curl_error( $curl );
+		}
 
-        curl_close( $curl );
+		curl_close( $curl );
 
-        return $response;
-    }
+		return $response;
+	}
 
-    function save ( $data, $filename = 'videos.json' )
-    {
-        try {
-            $fp = fopen( $this->folder . '/' . $filename, 'w' );
-            fwrite( $fp, $data );
-            fclose( $fp );
+	function save( $data, $filename = 'videos.json' )
+	{
+		try {
+			$fp = fopen( $this->folder . '/' . $filename, 'w' );
+			fwrite( $fp, $data );
+			fclose( $fp );
 
-            return true;
-        } catch ( Exception $e ) {
-            return false;
-        }
-    }
+			return true;
+		} catch ( Exception $e ) {
+			return false;
+		}
+	}
 
-    function read ( $filename = 'videos.json', $filter = array() )
-    {
-        //$cache = get_transient( 'at_import_big7_json_cache' );
+	function read( $filename = 'videos.json', $filter = array() )
+	{
+		//$cache = get_transient( 'at_import_big7_json_cache' );
 
-        //if(!file_exists($this->folder . '/' . $filename) || $cache === false) {
-        /**
-         * Try to get the file from Big7 directly
-         */
-        if ( $filename == 'videos.json' ) {
-            $data = file_get_contents( $this->videos );
-        } else {
-            $data = file_get_contents( $this->amateure );
-        }
+		//if(!file_exists($this->folder . '/' . $filename) || $cache === false) {
+		/**
+		 * Try to get the file from Big7 directly
+		 */
+		if ( $filename == 'videos.json' ) {
+			$data = file_get_contents( $this->videos );
+		} else {
+			$data = file_get_contents( $this->amateure );
+		}
 
-        if ( $data ) {
-            $this->save( $data, $filename );
-            //set_transient( 'at_import_big7_json_cache', 'cached' );
-        }
-        //}
+		if ( $data ) {
+			$this->save( $data, $filename );
+			//set_transient( 'at_import_big7_json_cache', 'cached' );
+		}
+		//}
 
-        set_time_limit( 0 );
+		set_time_limit( 0 );
 
-        $parser = new \JsonCollectionParser\Parser();
-        $parser->parse( $this->folder . '/' . $filename, [ $this,
-            'json_callback' ], true );
+		$parser = new \JsonCollectionParser\Parser();
+		$parser->parse( $this->folder . '/' . $filename, [ $this,
+			'json_callback' ], true );
 
-        $json_data = $this->json_result;
+		$json_data = $this->json_result;
 
-        if ( $json_data ) {
-            if ( $filter ) {
-                $filtered_data = array();
+		if ( $json_data ) {
+			if ( $filter ) {
+				$filtered_data = array();
 
-                foreach ( $filter as $key => $value ) {
-                    foreach ( $json_data as $k => $v ) {
-                        if ( $v[$key] == $value ) {
-                            $filtered_data[] = $v;
-                        }
-                    }
-                }
+				foreach ( $filter as $key => $value ) {
+					foreach ( $json_data as $k => $v ) {
+						if ( $v[ $key ] == $value ) {
+							$filtered_data[] = $v;
+						}
+					}
+				}
 
-                return $filtered_data;
-            }
+				return $filtered_data;
+			}
 
-            return $json_data;
-        }
+			return $json_data;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
 
-    function getAmateur ( $uid )
-    {
-        global $wpdb;
+	function getAmateur( $uid )
+	{
+		global $wpdb;
 
-        $database = new AT_Import_Big7_DB();
+		$database = new AT_Import_Big7_DB();
 
-        $data = $wpdb->get_results(
-            "
+		$data = $wpdb->get_results(
+			"
     		SELECT * FROM {$database->table_amateurs}
     	  	WHERE uid = {$uid}
     		",
-            OBJECT
-        );
+			OBJECT
+		);
 
-        if ( $data ) {
-            return $data[0];
-        }
+		if ( $data ) {
+			return $data[0];
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    function getAmateurName ( $uid )
-    {
-        global $wpdb;
+	function getAmateurName( $uid )
+	{
+		global $wpdb;
 
-        $database = new AT_Import_Big7_DB();
+		$database = new AT_Import_Big7_DB();
 
-        $data = $wpdb->get_var(
-            "
+		$data = $wpdb->get_var(
+			"
     		SELECT username FROM {$database->table_amateurs}
     	  	WHERE uid = {$uid}
     		"
-        );
+		);
 
-        if ( $data ) {
-            return $data;
-        }
+		if ( $data ) {
+			return $data;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    function getVideos ( $uid )
-    {
-        global $wpdb;
+	function getVideos( $uid )
+	{
+		global $wpdb;
 
-        $database = new AT_Import_Big7_DB();
+		$database = new AT_Import_Big7_DB();
 
-        $data = $wpdb->get_results(
-            "
+		$data = $wpdb->get_results(
+			"
     		SELECT * FROM {$database->table_videos}
     	  	WHERE uid = {$uid}
     		",
-            OBJECT
-        );
+			OBJECT
+		);
 
-        if ( $data ) {
-            return $data;
-        }
+		if ( $data ) {
+			// sanitize categories
+			$Crawler    = new AT_Import_Big7_Crawler();
+			$categories = $Crawler->getCategories();
+			$fsk18      = get_option( 'at_big7_fsk18' );
 
-        return false;
-    }
+			if ( $categories && $fsk18 ) {
+				foreach ( $data as $k => $v ) {
+					$itemCategories = explode( ',', $v->categories );
 
-    function getVideosByCategory ( $category, $offset = 0, $limit = 100 )
-    {
-        global $wpdb;
+					if ( $itemCategories ) {
+						foreach ( $itemCategories as $key => $value ) {
+							foreach ( $categories as $term ) {
+								if ( $value == $term['name_softcore'] ) {
+									$itemCategories[ $key ] = $term['name'];
+								}
+							}
+						}
 
-        $database = new AT_Import_Big7_DB();
+						$data[ $k ]->categories = implode( ',', $itemCategories );
+					}
+				}
+			}
 
-        $data = $wpdb->get_results(
-            "
+			return $data;
+		}
+
+		return false;
+	}
+
+	function getVideosByCategory( $category, $offset = 0, $limit = 100 )
+	{
+		global $wpdb;
+
+		$database = new AT_Import_Big7_DB();
+
+		$data = $wpdb->get_results(
+			"
             SELECT * FROM {$database->table_videos}
             WHERE categories LIKE '%{$category}%'
             LIMIT {$offset}, {$limit}
             ",
-            OBJECT
-        );
+			OBJECT
+		);
 
-        if ( $data ) {
-            return $data;
-        }
+		if ( $data ) {
+			// sanitize categories
+			$Crawler    = new AT_Import_Big7_Crawler();
+			$categories = $Crawler->getCategories();
+			$fsk18      = get_option( 'at_big7_fsk18' );
 
-        return false;
-    }
+			if ( $categories && $fsk18 ) {
+				foreach ( $data as $k => $v ) {
+					$itemCategories = explode( ',', $v->categories );
 
-    function jsonAmateurs ()
-    {
-        $data = $this->read( 'amateure.json' );
+					if ( $itemCategories ) {
+						foreach ( $itemCategories as $key => $value ) {
+							foreach ( $categories as $term ) {
+								if ( $value == $term['name_softcore'] ) {
+									$itemCategories[ $key ] = $term['name'];
+								}
+							}
+						}
 
-        if ( $data ) {
-            return $data;
-        }
+						$data[ $k ]->categories = implode( ',', $itemCategories );
+					}
+				}
+			}
 
-        return false;
-    }
+			return $data;
+		}
 
-    function jsonVideos ()
-    {
-        $data = $this->read( 'videos.json' );
+		return false;
+	}
 
-        if ( $data ) {
-            return $data;
-        }
+	function jsonAmateurs()
+	{
+		$data = $this->read( 'amateure.json' );
 
-        return false;
-    }
+		if ( $data ) {
+			return $data;
+		}
+
+		return false;
+	}
+
+	function jsonVideos()
+	{
+		$data = $this->read( 'videos.json' );
+
+		if ( $data ) {
+			return $data;
+		}
+
+		return false;
+	}
+
+	function getCategories()
+	{
+		$categories = get_transient( 'at_import_big7_categories' );
+
+		if ( ! $categories ) {
+			$jsonData = file_get_contents( $this->categories );
+
+			if ( $jsonData ) {
+				$categories = json_decode( $jsonData, true );
+				set_transient( 'at_import_big7_categories', $categories, 72 * HOUR_IN_SECONDS );
+			}
+		}
+
+		return $categories;
+	}
 }
--- modules/import/big7/helper.php
+++ modules/import/big7/helper.php
@@ -8,318 +8,299 @@
  */
 
 if ( ! function_exists( 'at_import_big7_scripts' ) ) {
-    /**
-     * at_import_big7_scripts function
-     *
-     */
-    add_action( 'admin_enqueue_scripts', 'at_import_big7_scripts' );
-    function at_import_big7_scripts ( $page )
-    {
-        if ( strpos( $page, 'at_import_big7' ) === false ) {
-            return;
-        }
+	/**
+	 * at_import_big7_scripts function
+	 *
+	 */
+	add_action( 'admin_enqueue_scripts', 'at_import_big7_scripts' );
+	function at_import_big7_scripts( $page )
+	{
+		if ( strpos( $page, 'at_import_big7' ) === false ) {
+			return;
+		}
 
-        wp_enqueue_script( 'at-big7', get_template_directory_uri() . '/modules/import/_assets/js/big7.js' );
-    }
+		wp_enqueue_script( 'at-big7', get_template_directory_uri() . '/modules/import/_assets/js/big7.js' );
+	}
 }
 
 if ( ! function_exists( 'at_import_big7_prepare_video_fields' ) ) {
-    /**
-     * at_import_big7_prepare_video_fields
-     *
-     * @param $video_id
-     *
-     * @return array|bool
-     */
-    function at_import_big7_prepare_video_fields ( $data )
-    {
-        if ( $data ) {
-            // format duration
-            $raw_duration = preg_replace( '/\D/', '', $data->duration );
-            $duration     = gmdate( "H:i:s", $raw_duration );
+	/**
+	 * at_import_big7_prepare_video_fields
+	 *
+	 * @param $video_id
+	 *
+	 * @return array|bool
+	 */
+	function at_import_big7_prepare_video_fields( $data )
+	{
+		if ( $data ) {
+			// format duration
+			$raw_duration = preg_replace( '/\D/', '', $data->duration );
+			$duration     = gmdate( "H:i:s", $raw_duration );
 
-            // format date
-            $raw_date = $data->date;
-            $date     = date( 'Ymd', strtotime( $raw_date ) );
+			// format date
+			$raw_date = $data->date;
+			$date     = date( 'Ymd', strtotime( $raw_date ) );
 
-            // format rating
-            $raw_rating = $data->rating / 10;
-            $rating     = $rating = round( ( $raw_rating / 2 ) * 2 ) / 2;
+			// format rating
+			$raw_rating = $data->rating / 10;
+			$rating     = $rating = round( ( $raw_rating / 2 ) * 2 ) / 2;
 
-            $fields = array(
-                'duration'     => $duration,
-                'views'        => 0,
-                'date'         => $date,
-                'rating'       => $rating,
-                'rating_count' => $data->rating_count,
-                'url'          => $data->link,
-                'source'       => 'big7',
-                'language'     => 'de',
-                'preview_webm' => $data->preview_webm,
-                'preview_mp4'  => $data->preview_mp4,
-                'unique_id'    => $data->video_id
-            );
+			$fields = array(
+				'duration'     => $duration,
+				'views'        => 0,
+				'date'         => $date,
+				'rating'       => $rating,
+				'rating_count' => $data->rating_count,
+				'url'          => $data->link,
+				'source'       => 'big7',
+				'language'     => 'de',
+				'preview_webm' => $data->preview_webm,
+				'preview_mp4'  => $data->preview_mp4,
+				'unique_id'    => $data->video_id
+			);
 
-            return $fields;
-        }
+			return $fields;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
 
 if ( ! function_exists( 'at_import_big7_get_video_count' ) ) {
-    /**
-     * at_import_big7_get_video_count
-     *
-     * @param $source_id
-     * @param bool $imported
-     *
-     * @return string
-     */
-    function at_import_big7_get_video_count ( $source_id )
-    {
-        global $wpdb;
+	/**
+	 * at_import_big7_get_video_count
+	 *
+	 * @param $source_id
+	 * @param bool $imported
+	 *
+	 * @return string
+	 */
+	function at_import_big7_get_video_count( $source_id )
+	{
+		global $wpdb;
 
-        $database = new AT_Import_Big7_DB();
+		$database = new AT_Import_Big7_DB();
 
-        $videos = $wpdb->get_var( 'SELECT COUNT(id) as count FROM ' . $database->table_videos . ' WHERE uid = ' . $source_id );
+		$videos = $wpdb->get_var( 'SELECT COUNT(id) as count FROM ' . $database->table_videos . ' WHERE uid = ' . $source_id );
 
-        if ( $videos ) {
-            return $videos;
-        }
+		if ( $videos ) {
+			return $videos;
+		}
 
-        return '0';
-    }
+		return '0';
+	}
 }
 
 if ( ! function_exists( 'at_import_big7_get_imported_video_count' ) ) {
-    /**
-     * at_import_big7_get_imported_video_count
-     *
-     * @param $source_id
-     * @param bool $imported
-     *
-     * @return string
-     */
-    function at_import_big7_get_imported_video_count ( $source_id )
-    {
-        global $wpdb;
+	/**
+	 * at_import_big7_get_imported_video_count
+	 *
+	 * @param $source_id
+	 * @param bool $imported
+	 *
+	 * @return string
+	 */
+	function at_import_big7_get_imported_video_count( $source_id )
+	{
+		global $wpdb;
 
-        $term = $wpdb->get_var( 'SELECT term_id FROM ' . $wpdb->termmeta . ' WHERE meta_value = "' . $source_id . '"' );
+		$term = $wpdb->get_var( 'SELECT term_id FROM ' . $wpdb->termmeta . ' WHERE meta_value = "' . $source_id . '"' );
 
-        if ( $term ) {
-            $item = get_term_by( 'id', $term, 'video_actor' );
-            if ( $item ) {
-                return $item->count;
-            }
-        }
+		if ( $term ) {
+			$item = get_term_by( 'id', $term, 'video_actor' );
+			if ( $item ) {
+				return $item->count;
+			}
+		}
 
-        return '0';
-    }
+		return '0';
+	}
 }
 
 if ( ! function_exists( 'at_import_big7_get_category_video_count' ) ) {
-    /**
-     * at_import_big7_get_video_category_count
-     *
-     * @param $source_id
-     * @param bool $imported
-     *
-     * @return string
-     */
-    function at_import_big7_get_category_video_count ( $category )
-    {
-        global $wpdb;
+	/**
+	 * at_import_big7_get_video_category_count
+	 *
+	 * @param $source_id
+	 * @param bool $imported
+	 *
+	 * @return string
+	 */
+	function at_import_big7_get_category_video_count( $category )
+	{
+		global $wpdb;
 
-        $database = new AT_Import_Big7_DB();
+		$database = new AT_Import_Big7_DB();
 
-        $videos = $wpdb->get_var( 'SELECT COUNT(id) as count FROM ' . $database->table_videos . ' WHERE categories LIKE "%' . $category . '%"' );
+		$videos = $wpdb->get_var( 'SELECT COUNT(id) as count FROM ' . $database->table_videos . ' WHERE categories LIKE "%' . $category . '%"' );
 
-        if ( $videos ) {
-            return $videos;
-        }
+		if ( $videos ) {
+			return $videos;
+		}
 
-        return '0';
-    }
+		return '0';
+	}
 }
 
 if ( ! function_exists( 'at_import_big7_get_category_imported_video_count' ) ) {
-    /**
-     * at_import_big7_get_category_imported_video_count
-     *
-     * @param $source_id
-     * @param bool $imported
-     *
-     * @return string
-     */
-    function at_import_big7_get_category_imported_video_count ( $category )
-    {
-        global $wpdb;
+	/**
+	 * at_import_big7_get_category_imported_video_count
+	 *
+	 * @param $source_id
+	 * @param bool $imported
+	 *
+	 * @return string
+	 */
+	function at_import_big7_get_category_imported_video_count( $category )
+	{
+		global $wpdb;
 
-        $term = get_term_by( 'name', $category, 'video_actor' );
+		$term = get_term_by( 'name', $category, 'video_actor' );
 
-        if ( $term ) {
-            return $term->count;
-        }
+		if ( $term ) {
+			return $term->count;
+		}
 
-        return '0';
-    }
+		return '0';
+	}
 }
 
 if ( ! function_exists( 'at_import_big7_get_categories' ) ) {
-    /**
-     * at_import_big7_get_categories
-     *
-     * @return array|bool
-     */
-    function at_import_big7_get_categories ()
-    {
-        global $wpdb;
+	/**
+	 * at_import_big7_get_categories
+	 *
+	 * @return array|bool
+	 */
+	function at_import_big7_get_categories()
+	{
+		$Crawler    = new AT_Import_Big7_Crawler();
+		$categories = $Crawler->getCategories();
+		$fsk18      = get_option( 'at_big7_fsk18' );
+		$results    = [];
 
-        $database = new AT_Import_Big7_DB();
+		if ( $categories ) {
+			foreach ( $categories as $term ) {
+				$results[ $term['id'] ] = $fsk18 ? $term['name'] : $term['name_softcore'];
+			}
+		}
 
-        $data = $wpdb->get_var(
-            "
-			SELECT
-			  GROUP_CONCAT( DISTINCT SUBSTRING_INDEX(SUBSTRING_INDEX(categories, ',', n.digit+1), ',', -1)) categories
-			FROM
-			  " . $database->table_videos . "
-			  INNER JOIN
-			  (SELECT 0 digit UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3  UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6) n
-			  ON LENGTH(REPLACE(categories, ',' , '')) <= LENGTH(categories)-n.digit
-			"
-        );
-
-        if ( $data ) {
-            $items   = explode( ',', $data );
-            $results = array();
-
-            if ( $items ) {
-                foreach ( $items as $item ) {
-                    $unique           = substr( base_convert( md5( $item ), 16, 10 ), -10 );
-                    $results[$unique] = $item;
-                }
-            }
-
-            return $results;
-        }
-
-        return false;
-    }
+		return $results;
+	}
 }
 
 if ( ! function_exists( 'at_import_json_process' ) ) {
-    /**
-     * at_import_json_process
-     *
-     * @param $item
-     */
-    function at_import_json_process ( $item )
-    {
-        print_r( $item );
-    }
+	/**
+	 * at_import_json_process
+	 *
+	 * @param $item
+	 */
+	function at_import_json_process( $item )
+	{
+		print_r( $item );
+	}
 }
 
-function at_import_big7_update_actor ( $actor_id )
+function at_import_big7_update_actor( $actor_id )
 {
-    global $wpdb;
+	global $wpdb;
 
-    $actor_need_update = $wpdb->get_var( 'SELECT term_id FROM cp_termmeta WHERE meta_key = "actor_id" AND meta_value = "' . $actor_id . '" AND (SELECT term_id FROM cp_termmeta WHERE meta_key="actor_last_updated" AND meta_value < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) LIMIT 0,1)' );
+	$actor_need_update = $wpdb->get_var( 'SELECT term_id FROM cp_termmeta WHERE meta_key = "actor_id" AND meta_value = "' . $actor_id . '" AND (SELECT term_id FROM cp_termmeta WHERE meta_key="actor_last_updated" AND meta_value < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) LIMIT 0,1)' );
 
-    if ( $actor_need_update ) {
-        // get actor data
-        $import = new AT_Import_Big7_Crawler();
-        $actor  = $import->getAmateur( $actor_id );
+	if ( $actor_need_update ) {
+		// get actor data
+		$import = new AT_Import_Big7_Crawler();
+		$actor  = $import->getAmateur( $actor_id );
 
-        if ( $actor ) {
-            $post_id = 'video_actor_' . $actor_need_update;
+		if ( $actor ) {
+			$post_id = 'video_actor_' . $actor_need_update;
 
-            // image
-            $actor_image = get_field( 'actor_image', $post_id );
-            if ( ! $actor_image ) {
-                if ( $image = ( $actor->image_large ? $actor->image_large : '' ) ) {
-                    $att_id = at_attach_external_image( $image, null, false, $actor->username . '-preview', array( 'post_title' => $actor->username ) );
-                    if ( $att_id ) {
-                        update_field( 'actor_image', $att_id, $post_id );
-                    }
-                }
-            }
+			// image
+			$actor_image = get_field( 'actor_image', $post_id );
+			if ( ! $actor_image ) {
+				if ( $image = ( $actor->image_large ? $actor->image_large : '' ) ) {
+					$att_id = at_attach_external_image( $image, null, false, $actor->username . '-preview', array( 'post_title' => $actor->username ) );
+					if ( $att_id ) {
+						update_field( 'actor_image', $att_id, $post_id );
+					}
+				}
+			}
 
-            // gender
-            if ( $gender = $actor->gender ) {
-                if ( $gender == 'ts' ) {
-                    $gender_decoded = __( 'Transexuell', 'amateurtheme' );
-                } elseif ( $gender == 'w' ) {
-                    $gender_decoded = __( 'Weiblich', 'amateurtheme' );
-                } else {
-                    $gender_decoded = __( 'Männlich', 'amateurtheme' );
-                }
+			// gender
+			if ( $gender = $actor->gender ) {
+				if ( $gender == 'ts' ) {
+					$gender_decoded = __( 'Transexuell', 'amateurtheme' );
+				} elseif ( $gender == 'w' ) {
+					$gender_decoded = __( 'Weiblich', 'amateurtheme' );
+				} else {
+					$gender_decoded = __( 'Männlich', 'amateurtheme' );
+				}
 
-                update_field( 'actor_gender', $gender_decoded, $post_id );
-            }
+				update_field( 'actor_gender', $gender_decoded, $post_id );
+			}
 
-            // zipcode
-            if ( $zipcode = $actor->zipcode ) {
-                update_field( 'actor_zipcode', $zipcode, $post_id );
-            }
+			// zipcode
+			if ( $zipcode = $actor->zipcode ) {
+				update_field( 'actor_zipcode', $zipcode, $post_id );
+			}
 
-            // city
-            if ( $city = $actor->city ) {
-                update_field( 'actor_city', $city, $post_id );
-            }
+			// city
+			if ( $city = $actor->city ) {
+				update_field( 'actor_city', $city, $post_id );
+			}
 
-            // country
-            if ( $country = $actor->country ) {
-                update_field( 'actor_country', $country, $post_id );
-            }
+			// country
+			if ( $country = $actor->country ) {
+				update_field( 'actor_country', $country, $post_id );
+			}
 
-            // eyecolor
-            if ( $eyecolor = $actor->eyecolor ) {
-                update_field( 'actor_eyecolor', $eyecolor, $post_id );
-            }
+			// eyecolor
+			if ( $eyecolor = $actor->eyecolor ) {
+				update_field( 'actor_eyecolor', $eyecolor, $post_id );
+			}
 
-            // haircolor
-            if ( $haircolor = $actor->haircolor ) {
-                update_field( 'actor_haircolor', $haircolor, $post_id );
-            }
+			// haircolor
+			if ( $haircolor = $actor->haircolor ) {
+				update_field( 'actor_haircolor', $haircolor, $post_id );
+			}
 
-            // body
-            if ( $body = $actor->body ) {
-                update_field( 'actor_bodytype', $body, $post_id );
-            }
+			// body
+			if ( $body = $actor->body ) {
+				update_field( 'actor_bodytype', $body, $post_id );
+			}
 
-            // sex
-            if ( $sex = $actor->sex ) {
-                update_field( 'actor_sex_orientation', $sex, $post_id );
-            }
+			// sex
+			if ( $sex = $actor->sex ) {
+				update_field( 'actor_sex_orientation', $sex, $post_id );
+			}
 
-            // weight
-            if ( $weight = $actor->weight ) {
-                update_field( 'actor_weight', $weight, $post_id );
-            }
+			// weight
+			if ( $weight = $actor->weight ) {
+				update_field( 'actor_weight', $weight, $post_id );
+			}
 
-            // shaved
-            if ( $shaved = $actor->shaved ) {
-                update_field( 'actor_shaved', __( 'Ja', 'amateurtheme' ), $post_id );
-            }
+			// shaved
+			if ( $shaved = $actor->shaved ) {
+				update_field( 'actor_shaved', __( 'Ja', 'amateurtheme' ), $post_id );
+			}
 
-            // link
-            if ( $link = $actor->link ) {
-                update_field( 'actor_profile_url', $link, $post_id );
-            }
+			// link
+			if ( $link = $actor->link ) {
+				update_field( 'actor_profile_url', $link, $post_id );
+			}
 
-            // aboutme
-            if ( $aboutme = ( get_option( 'at_big7_fsk18' ) == 1 ? $actor->aboutme : $actor->aboutme_sc ) ) {
-                wp_update_term( $actor_need_update, 'video_actor', array(
-                    'description' => $aboutme,
-                ) );
-            }
+			// aboutme
+			if ( $aboutme = ( get_option( 'at_big7_fsk18' ) == 1 ? $actor->aboutme : $actor->aboutme_sc ) ) {
+				wp_update_term( $actor_need_update, 'video_actor', array(
+					'description' => $aboutme,
+				) );
+			}
 
-            update_field( 'actor_last_updated', time(), $post_id );
+			update_field( 'actor_last_updated', time(), $post_id );
 
-            at_error_log( 'Updated Actor: ' . $actor->username . ' (' . $actor_need_update . ')' );
-        }
-    }
+			at_error_log( 'Updated Actor: ' . $actor->username . ' (' . $actor_need_update . ')' );
+		}
+	}
 }
 
 /**
@@ -332,14 +313,14 @@ function at_import_big7_update_actor ( $actor_id )
  * @return string
  */
 add_filter( 'acf/load_value/name=video_url', 'xcore_big7_campaign_urls', 10, 3 );
-function xcore_big7_campaign_urls ( $value, $post_id, $field )
+function xcore_big7_campaign_urls( $value, $post_id, $field )
 {
-    if ( get_field( 'video_source', $post_id ) == 'big7' ) {
-        $campaign = get_option( 'at_big7_video_campaign' );
-        if ( $campaign ) {
-            $value .= '&wms=' . $campaign;
-        }
-    }
+	if ( get_field( 'video_source', $post_id ) == 'big7' ) {
+		$campaign = get_option( 'at_big7_video_campaign' );
+		if ( $campaign ) {
+			$value .= '&wms=' . $campaign;
+		}
+	}
 
-    return $value;
+	return $value;
 }
